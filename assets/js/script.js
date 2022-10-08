721526f6d672ab5acd71a84c1ae2bbcd407f197c
Aneela-Codes <https://github.com/Aneela-Codes/>
2022-10-08 21:34:47 +0500

    adding bg images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 
 const button = document.getElementById('widget-btn');
-isOpen = true
+const showPrice = document.getElementById('showPrice');
+const priceBtns = document.querySelectorAll('.price')
+
+isOpen = false
 function showMenu() {
 
     if(isOpen == true){
@@ -15,4 +18,18 @@ function showMenu() {
     }
 
 }
-console.log(button);
+
+// for (var i = 0; i < priceBtns.length; i++) {
+//     priceBtns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//     console.log('yo');
+
+//     });
+//   }
+function selectValue(int){
+
+    showPrice.value = int
+}
+
